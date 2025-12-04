ce1e0628bd999fcfc047f0c632fa378341c03e8d
최세형 <opde20@yonsei.ac.kr>
2025-12-04 14:28:38 +0900

    Add test c program

---
+++ choisehyung.c
@@ -0,0 +1,11 @@
+#include <sdtio.h>
+int main(){
+	print("Hello Git Test\n");
+	return 0;
+}[A[C[C[C[C[C[A[A[A[C[B[C[C[C [B[B[B[B[D[D[D[D[D[D[D[D[D[D
+
+$ls
+
+
+[A[A[A[C[C[C[1;2D[1;2D[1;2D[1;2D[C[C[[[A[A[A[B$ ls
+[A[C[C[[A[A[A[A[A[A[A[A[C[B[C[C[C[C[C[C[C[C[C[C[Ctd[B[B[B[D[A[Cf[B[[2~[C([[[B[A[C[3[C[2~[Cdd[2~ddHello[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[2~
