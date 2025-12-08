e6d5e2aea7584529efe1199687310285e2b80e45
백승우 <bsw020309@gmail.com>
2025-12-08 21:27:23 +0900

    Add test c program

--- baekseungwoo.c
+++ baekseungwoo.c
@@ -1,5 +1,5 @@
-#include <>[Dstdio.h>[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[3~[3~[3~[3~[3~
+#include <stdio.h>
 int main() {
-	print("Hellow Git Test");
-	return 0;
+    printf("Hello Git Test");
+    return 0;
 }
