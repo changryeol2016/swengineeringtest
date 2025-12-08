419e4b1c9c0b5e9d93c14d971239ce1e0cda0fdc
백승우 <bsw020309@gmail.com>
2025-12-08 21:21:27 +0900

    Add test c program

---
+++ baekseungwoo.c
@@ -0,0 +1,5 @@
+#include <>[Dstdio.h>[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[3~[3~[3~[3~[3~
+int main() {
+	print("Hellow Git Test");
+	return 0;
+}
