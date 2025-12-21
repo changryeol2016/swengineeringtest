44e0cb10985886a6fc658f0fc09c2ff741a3258f
김루하 <musesousvois@yonsei.ac.kr>
2025-12-21 21:13:25 +0900

    Add test c program

---
+++ kimluha.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	print()[D"Hello, Git Test\n")[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Df("Hello, Git Test\n")[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[[("Hello, Git Test\n")
+	return 0;
+}
+
